07ff59a3765ac596a4852235597019239588a9c1
kittchy <kinouchi.takahiro.tg@tut.jp>
2023-09-03 00:44:23 +0900

    [ADD] delete 機能の追加

--- app/diary/users/delete_entry.py
+++ app/diary/users/delete_entry.py
@@ -1,4 +1,20 @@
 import json
+from decimal import Decimal
+
+import boto3
+import os
+# from boto3.dynamodb.conditions import Key
+
+
+dynamodb = boto3.resource("dynamodb", region_name="ap-northeast-1")
+user_diary_table = dynamodb.Table(f"UserDiaryTable-{PR_NUM}")
+
+
+def decimal_to_int(obj):
+    if isinstance(obj, Decimal):
+        return int(obj)
+    if isinstance(obj, set):
+        return list(obj)
 
 
 def lambda_handler(event, context):
@@ -6,13 +22,27 @@ def lambda_handler(event, context):
     diary_id = event.get("pathParameters", {}).get("diary_id")
 
     # バリデーション
-    if user_id is None or not isinstance(user_id, int):
+    if user_id is None or not isinstance(user_id, str):
         return {"statusCode": 400, "body": "Bad Request: Invalid user_id"}
     if diary_id is None or not isinstance(diary_id, str):
         return {"statusCode": 400, "body": "Bad Request: Invalid diary_id"}
 
-    # ここに処理を書く
+    # search user diary
+    option = {"Key": {"diary_id": diary_id}}
+    response = user_diary_table.get_item(**option)
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        return {"statusCode": 500, "body": "DynamoDB Error"}
+    if "Item" not in response:
+        return {"statusCode": 404, "body": "TaskID is not found"}
 
+    # delete user diary
+    option = {"Key": {"diary_id": diary_id}}
+    response = user_diary_table.delete_item(**option)
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        return {
+            "statusCode": 500,
+            "body": f"Failed to delete diary with ID: {diary_id} for section with ID: {user_id}",
+        }
     return {
         "statusCode": 200,
         "body": json.dumps(
