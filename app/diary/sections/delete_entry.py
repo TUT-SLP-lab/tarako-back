bb68e135efcaa7043f0a5bbdcb51e295a550fafa
kittchy <kinouchi.takahiro.tg@tut.jp>
2023-09-04 00:00:45 +0900

    [ADD] delete source code

--- app/diary/sections/delete_entry.py
+++ app/diary/sections/delete_entry.py
@@ -1,9 +1,19 @@
 import json
 
+from layer.table_utils import section_diary_table
+
 
 def lambda_handler(event, context):
-    section_id = event.get("pathParameters", {}).get("section_id")
-    diary_id = event.get("pathParameters", {}).get("diary_id")
+    ppm = event.get("pathParameters", {})
+    if ppm is None:
+        return {"statusCode": 400, "body": "Bad Request: Invalid path parameters"}
+    sections_id = ppm.get("sections_id", None)
+    diary_id = ppm.get("diary_id")
+
+    try:
+        section_id = int(sections_id)
+    except ValueError:
+        return {"statusCode": 400, "body": "Bad Request: Invalid section_id"}
 
     # バリデーション
     if section_id is None or not isinstance(section_id, int):
@@ -11,7 +21,29 @@ def lambda_handler(event, context):
     if diary_id is None or not isinstance(diary_id, str):
         return {"statusCode": 400, "body": "Bad Request: Invalid diary_id"}
 
-    # ここに処理を書く
+    option = {"Key": {"section_id": section_id}}
+    response = section_diary_table.get_item(**option)
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        return {
+            "statusCode": 500,
+            "body": f"Failed to find diary with section id: {section_id}",
+        }
+    if "Item" not in response:
+        return {
+            "statusCode": 404,
+            "body": f"Item is not found with {section_id}",
+        }
+    if response["Item"]["diary_id"] != diary_id:
+        return {"statsCode": 404, "body": f"Failed to fild diary_id: {diary_id}"}
+
+    option = {"Key": {"diary_id": diary_id}}
+    response = section_diary_table.delete_item(**option)
+
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        return {
+            "statusCode": 500,
+            "body": f"Failed to delete diary with ID: {diary_id} for section with ID: {section_id}",
+        }
 
     return {
         "statusCode": 200,
