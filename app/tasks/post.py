cb27056822094a73ca5ea9e7c253803051983665
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-04 01:05:36 +0900

    update: タスクの詳細取得APIを実装
    internal server errorが起こるため、要修正

--- app/tasks/post.py
+++ app/tasks/post.py
@@ -1,8 +1,10 @@
-import json
+import cgi
+import random
 import uuid
 from datetime import datetime
+from io import BytesIO
 
-from table_utils import json_dumps, task_table
+from table_utils import json_dumps, task_table, validate_file
 
 category_list = [
     "HR",
@@ -14,68 +16,97 @@ category_list = [
 
 
 def lambda_handler(event, context):
-    ppm = event.get("pathParameters")
-    if ppm:
-        user_id = ppm.get("user_id")
     qsp = event.get("queryStringParameters")
     if qsp:
         force_create = qsp.get("force_create", None)
     else:
-        force_create = None
-    body = json.loads(event.get("body", None))
+        force_create = None  # suggest_taskを使用する際に必要
+    body = event.get("body")
+    if body is None:
+        return {
+            "statusCode": 400,
+            "body": json_dumps({"error": "Missing request body"}),
+        }
 
-    if user_id is None or not isinstance(user_id, str):
-        return {"statusCode": 400, "body": "Bad Request: Invalid user_id"}
-    # TODO: bodyの中身のバリデーションも書く
-    if not body:
-        return {"statusCode": 400, "body": "Bad Request: Empty request body"}
+    # BytesIOを使用してボディをファイルポインタとして扱う
+    body_file = BytesIO(body.encode("utf-8"))
 
-    task_id = str(uuid.uuid4())
-    msg = body.get("message")
-    # TODO: ファイルがアップロードされた際の処理も書く
+    # FieldStorageを使うと、multipart/form-dataの場合に対応できる
+    try:
+        form = cgi.FieldStorage(
+            fp=body_file,
+            environ={"REQUEST_METHOD": "POST"},
+            headers={"Content-Type": "multipart/form-data"},
+        )
+        user_id = form.getfirst("user_id", None)
+        msg = form.getfirst("text", None)
+        if "file" in form:
+            file_item = form["file"]
+            file_data = file_item.file.read()  # ファイルデータを読み込む
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 500,
+            "body": json_dumps({"error": "Failed to parse form-data"}),
+        }
 
-    gpt_output = gen_dummy_data()
+    # バリデーションの続き
+    error_strings = []
+    # TODO: user_idが存在するかどうかを確認する
+    if not user_id:
+        error_strings.append("user_id is required")
+    elif not isinstance(user_id, str):
+        error_strings.append("user_id must be string")
+    if not (msg or file):
+        error_strings.append("text or file is required")
+    if msg and not isinstance(msg, str):
+        error_strings.append("text must be string")
+    # fileはバイナリデータ
+    if file_item and validate_file(file_item):
+        error_strings.append("file must be unique")
+    elif len(file) == 1:
+        file = file[0]
+        if isinstance(file, str):
+            error_strings.append("file must be string")
+
+    # TODO: ファイルがアップロードされた際の処理を書く
+    if not msg:
+        error_strings.append("ファイルアップロードが未対応の間、textは必須です")
+
+    gpt_output = gen_dummy_data(msg)
 
     # TODO: 似たタスクがあるかどうかを確認する
 
     now = datetime.now().isoformat()
+    task_id = str(uuid.uuid4())
     task = {
         "task_id": task_id,
-        "assigned_by": user_id,
-        "title": msg,
+        "assigned_to": user_id,
+        "title": gpt_output.get("title"),
         "category": gpt_output.get("category"),
         "tags": gpt_output.get("tags"),
-        "progresses": [
-            {
-                "datetime": now,
-                "percentage": gpt_output.get("progress"),
-            }
-        ],
+        "progresses": [{"datetime": now, "percentage": gpt_output.get("progress")}],
+        "started_at": now,
+        "last_status_at": now,
         "completed": gpt_output.get("progress") == 100,
         "serious": gpt_output.get("serious"),
-        "details": msg,
+        "details": gpt_output.get("details"),
+        "placeholder": "",  # NOTE: 検索のためのダミーフィールド。dynamodbの弊害
         "created_at": now,
         "updated_at": now,
     }
 
     # task_tableに追加する
     task_table.put_item(Item=task)
-    # task_tableから再取得する
-    task = task_table.get_item(Key={"task_id": task_id}).get("Item")
-
-    return {
-        "statusCode": 200,
-        "body": json_dumps(task),
-        "headers": {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "POST",
-            "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
-        },
-    }
+    return {"statusCode": 200, "body": json_dumps(task)}
 
 
 def check_suggest_task():
-    """タスクの提案を行うかどうかを判定する"""
+    """
+    タスクの提案を行うかどうかを判定する
+
+    備考: ChatGPTと合体するまで保留
+    """
     pass
 
 
@@ -92,11 +123,13 @@ dummy_tags = [
 ]
 
 
-def gen_dummy_data():
+def gen_dummy_data(msg):
     """ChatGPTから返ってくるであろう出力を生成する"""
     return {
+        "title": msg,
         "category": random.choice(category_list),
         "tags": random.sample(dummy_tags, random.randint(1, len(dummy_tags))),
         "progress": random.int(0, 100),
         "serious": random.randint(0, 5),
+        "details": msg,
     }
