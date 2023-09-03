9a086cb0e7e0bc6974582c38d9ea683acc6e54e6
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-03 22:47:00 +0900

    udpate: ひとまず、taskに投稿可能なようにした
    validationをこれから行う

--- app/tasks/post.py
+++ app/tasks/post.py
@@ -1,45 +1,102 @@
 import json
+import uuid
+from datetime import datetime
+
+from table_utils import json_dumps, task_table
+
+category_list = [
+    "HR",
+    "Accounting",
+    "GeneralAffairs",
+    "Diary",
+    "Other",
+]
 
 
 def lambda_handler(event, context):
+    ppm = event.get("pathParameters")
+    if ppm:
+        user_id = ppm.get("user_id")
     qsp = event.get("queryStringParameters")
     if qsp:
-        force_create = event.get("queryStringParameters", {}).get("force_create")
+        force_create = qsp.get("force_create", None)
     else:
         force_create = None
-    body = json.loads(event.get("body", "{}"))
+    body = json.loads(event.get("body", None))
 
-    # バリデーション
-    if force_create and not isinstance(force_create, bool):
-        return {"statusCode": 400, "body": "Bad Request: Invalid force_create"}
+    if user_id is None or not isinstance(user_id, str):
+        return {"statusCode": 400, "body": "Bad Request: Invalid user_id"}
+    # TODO: bodyの中身のバリデーションも書く
     if not body:
-        return {"statusCode": 400, "body": "Bad Request: Missing request body"}
+        return {"statusCode": 400, "body": "Bad Request: Empty request body"}
 
-    # ここに処理を書く
-    example = {
-        "task_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "assigned_by": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "title": "単体テスト作成",
-        "category": "HR",
-        "tags": ["人事", "休暇"],
+    task_id = str(uuid.uuid4())
+    msg = body.get("message")
+    # TODO: ファイルがアップロードされた際の処理も書く
+
+    gpt_output = gen_dummy_data()
+
+    # TODO: 似たタスクがあるかどうかを確認する
+
+    now = datetime.now().isoformat()
+    task = {
+        "task_id": task_id,
+        "assigned_by": user_id,
+        "title": msg,
+        "category": gpt_output.get("category"),
+        "tags": gpt_output.get("tags"),
         "progresses": [
-            {"datetime": "2020-01-01T00:00:00+09:00", "percentage": 0},
-            {"datetime": "2020-01-02T00:00:00+09:00", "percentage": 50},
-            {"datetime": "2020-01-03T00:00:00+09:00", "percentage": 100},
+            {
+                "datetime": now,
+                "percentage": gpt_output.get("progress"),
+            }
         ],
-        "completed": True,
-        "serious": 0,
-        "details": "hoge.fugaの単体テストを作成する",
-        "created_at": "2020-01-01T00:00:00+09:00",
-        "updated_at": "2020-01-01T00:00:00+09:00",
+        "completed": gpt_output.get("progress") == 100,
+        "serious": gpt_output.get("serious"),
+        "details": msg,
+        "created_at": now,
+        "updated_at": now,
     }
+
+    # task_tableに追加する
+    task_table.put_item(Item=task)
+    # task_tableから再取得する
+    task = task_table.get_item(Key={"task_id": task_id}).get("Item")
 
     return {
         "statusCode": 200,
-        "body": json.dumps(example),
+        "body": json_dumps(task),
         "headers": {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "POST",
             "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
         },
+    }
+
+
+def check_suggest_task():
+    """タスクの提案を行うかどうかを判定する"""
+    pass
+
+
+# HACK: ここから下は、ダミーデータを生成するための諸々
+# 本番までに取り除きたい
+
+dummy_tags = [
+    "人事",
+    "休暇",
+    "経理",
+    "総務",
+    "日報",
+    "その他",
+]
+
+
+def gen_dummy_data():
+    """ChatGPTから返ってくるであろう出力を生成する"""
+    return {
+        "category": random.choice(category_list),
+        "tags": random.sample(dummy_tags, random.randint(1, len(dummy_tags))),
+        "progress": random.int(0, 100),
+        "serious": random.randint(0, 5),
     }
