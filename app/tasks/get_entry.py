939f4c7b59968fe46a706b118119b79ff2f73101
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-03 21:18:46 +0900

    update: タスクの詳細取得APIを実装

--- app/tasks/get_entry.py
+++ app/tasks/get_entry.py
@@ -1,36 +1,23 @@
-import json
+from table_utils import json_dumps, task_table
 
 
 def lambda_handler(event, context):
-    task_id = event.get("pathParameters", {}).get("task_id")
+    ppm = event.get("pathParameters")
+    if ppm is None:
+        return {"statusCode": 400, "body": "Bad Request: Missing pathParameters"}
+    task_id = ppm.get("task_id")
 
     # バリデーション
     if not task_id:
         return {"statusCode": 400, "body": "Bad Request: Missing task_id"}
 
-    # ここに処理を書く
-    # TODO: task_idで検索をかける
-    example = {
-        "task_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "assigned_by": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "title": "単体テスト作成",
-        "category": "HR",
-        "tags": ["人事", "休暇"],
-        "progresses": [
-            {"datetime": "2020-01-01T00:00:00+09:00", "percentage": 0},
-            {"datetime": "2020-01-02T00:00:00+09:00", "percentage": 50},
-            {"datetime": "2020-01-03T00:00:00+09:00", "percentage": 100},
-        ],
-        "completed": True,
-        "serious": 0,
-        "details": "hoge.fugaの単体テストを作成する",
-        "created_at": "2020-01-01T00:00:00+09:00",
-        "updated_at": "2020-01-01T00:00:00+09:00",
-    }
+    response = task_table.get_item(Key={"task_id": task_id})
+    if "Item" not in response:
+        return {"statusCode": 404, "body": "Not Found"}
 
     return {
         "statusCode": 200,
-        "body": json.dumps(example),
+        "body": json_dumps(response["Item"]),
         "headers": {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "GET",
