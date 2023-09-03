242b16312816a9ef7a48c2fd90f1dc344a61f30f
kittchy <kinouchi.takahiro.tg@tut.jp>
2023-09-03 14:20:12 +0900

    [ADD] user diary POST APIの作成

--- app/diary/users/post_entry.py
+++ app/diary/users/post_entry.py
@@ -1,30 +1,64 @@
 import json
+import uuid
+from datetime import datetime
+
+from boto3.dynamodb.types import TypeSerializer
+from table_utils import json_dumps, user_diary_table
+
+serializer = TypeSerializer()
 
 
 def lambda_handler(event, context):
-    user_id = event.get("pathParameters", {}).get("user_id")
-    body = json.loads(event.get("body", "{}"))
+    path_params = event.get("pathParameters", {})
+    if path_params is None:
+        return {"statusCode": 400, "body": "Bad Request: Missing path parameters"}
+    user_id = path_params.get("user_id", None)
 
+    body = event.get("body", {})
+    if body is None:
+        return {"statusCode": 400, "body": "Bad Request: Missing request body"}
+    body = json.loads(body)
+    date = body.get("date", None)
+    message = body.get("message", None)
+
+    # バリデーション
     if user_id is None or not isinstance(user_id, str):
         return {"statusCode": 400, "body": "Bad Request: Invalid user_id"}
-    if not body:
-        return {"statusCode": 400, "body": "Bad Request: Empty request body"}
+    if date is None or not isinstance(date, str):
+        return {"statusCode": 400, "body": "Bad Request: Invalid date"}
+    if message is None or not isinstance(message, str):
+        return {"statusCode": 400, "body": "Bad Request: Invalid message"}
 
-    # ここに処理を書く
-    example = {
-        "diary_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-        "date": "2023-08-31",
-        "details": "hoge.fugaの単体テストを作成する",
+    # TODO from user db
+    user_list = [
+        "4f73ab32-21bf-47ef-a119-fa024bc2b9cc",
+        "595c060d-8417-4ac8-bcb5-c8e733dc64e0",
+        "e08bf311-b1bc-4a38-bac1-374c3ede7203",
+    ]
+    if user_id not in user_list:
+        return {"statusCode": 400, "body": "Bad Request: user_id not found"}
+
+    # get from task
+    # TODO send message to ChatGPT
+    diary_id = str(uuid.uuid4())
+    item = {
+        "diary_id": diary_id,
+        "section_id": 1,
+        "date": date,
+        "details": "hogehoge",
         "serious": 0,
-        "task_ids": ["3fa85f64-5717-4562-b3fc-2c963f66afa6"],
-        "created_at": "2020-01-01T00:00:00+09:00",
-        "updated_at": "2020-01-01T00:00:00+09:00",
-        "user_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        "created_at": datetime.now().isoformat(),
+        "updated_at": datetime.now().isoformat(),
+        "user_id": user_id,
+        "task_ids": ["aaaa", "aaa"],
     }
-
+    response = user_diary_table.put_item(Item=item)
+    if response["ResponseMetadata"]["HTTPStatusCode"] != 200:
+        return {"statusCode": 500, "body": "Internal Server Error"}
+    response = user_diary_table.get_item(Key={"diary_id": diary_id})
     return {
         "statusCode": 200,
-        "body": json.dumps(example),
+        "body": json_dumps(response["Item"]),
         "headers": {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "POST",
