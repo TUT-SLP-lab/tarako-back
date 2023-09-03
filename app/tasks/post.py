d0981a24a09e154b2ec9171ce8b07db5d93880cc
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-04 02:04:00 +0900

    update: タスクの詳細取得APIを実装

--- app/tasks/post.py
+++ app/tasks/post.py
@@ -35,14 +35,19 @@ def lambda_handler(event, context):
     try:
         form = cgi.FieldStorage(
             fp=body_file,
-            environ={"REQUEST_METHOD": "POST"},
-            headers={"Content-Type": "multipart/form-data"},
+            environ={
+                "REQUEST_METHOD": "POST",
+                "CONTENT_TYPE": event.get("headers").get("Content-Type"),
+            },
         )
         user_id = form.getfirst("user_id", None)
         msg = form.getfirst("text", None)
         if "file" in form:
             file_item = form["file"]
             file_data = file_item.file.read()  # ファイルデータを読み込む
+        else:
+            file_item = None
+            file_data = None
     except Exception as e:
         print(e)
         return {
@@ -51,47 +56,59 @@ def lambda_handler(event, context):
         }
 
     # バリデーションの続き
-    error_strings = []
+    error_msg = []
     # TODO: user_idが存在するかどうかを確認する
-    if not user_id:
-        error_strings.append("user_id is required")
+    if user_id is None:
+        error_msg.append("user_id is required")
     elif not isinstance(user_id, str):
-        error_strings.append("user_id must be string")
-    if not (msg or file):
-        error_strings.append("text or file is required")
+        error_msg.append("user_id must be string")
+    if not (msg or file_item):
+        error_msg.append("text or file is required")
     if msg and not isinstance(msg, str):
-        error_strings.append("text must be string")
+        error_msg.append("text must be string")
     # fileはバイナリデータ
     if file_item and validate_file(file_item):
-        error_strings.append("file must be unique")
-    elif len(file) == 1:
-        file = file[0]
-        if isinstance(file, str):
-            error_strings.append("file must be string")
+        error_msg.append("file must be unique")
+
+    if error_msg:
+        return {
+            "statusCode": 400,
+            "body": json_dumps({"error": "\n".join(error_msg)}),
+        }
 
     # TODO: ファイルがアップロードされた際の処理を書く
     if not msg:
-        error_strings.append("ファイルアップロードが未対応の間、textは必須です")
+        error_msg.append("ファイルアップロードが未対応の間、textは必須です")
 
     gpt_output = gen_dummy_data(msg)
 
     # TODO: 似たタスクがあるかどうかを確認する
 
-    now = datetime.now().isoformat()
+    # NOTE: 本来はuser_idからsection_idを取得する
+    # 下剤は、userの直打ちから持ってきている
+    section_id_dict = {
+        "4f73ab32-21bf-47ef-a119-fa024bc2b9cc": 0,
+        "595c060d-8417-4ac8-bcb5-c8e733dc64e0": 0,
+        "e08bf311-b1bc-4a38-bac1-374c3ede7203": 1,
+    }
+    section_id = int(section_id_dict.get(user_id, 0))
+
+    now = str(datetime.now().isoformat())
     task_id = str(uuid.uuid4())
     task = {
         "task_id": task_id,
         "assigned_to": user_id,
+        "section_id": section_id,
         "title": gpt_output.get("title"),
         "category": gpt_output.get("category"),
         "tags": gpt_output.get("tags"),
         "progresses": [{"datetime": now, "percentage": gpt_output.get("progress")}],
         "started_at": now,
         "last_status_at": now,
-        "completed": gpt_output.get("progress") == 100,
+        "completed": "True" if gpt_output.get("progress") == 100 else "False",
         "serious": gpt_output.get("serious"),
         "details": gpt_output.get("details"),
-        "placeholder": "",  # NOTE: 検索のためのダミーフィールド。dynamodbの弊害
+        "placeholder": 0,  # NOTE: 検索のためのダミーフィールド。dynamodbの弊害 TODO: 空文字列に変更
         "created_at": now,
         "updated_at": now,
     }
@@ -129,7 +146,7 @@ def gen_dummy_data(msg):
         "title": msg,
         "category": random.choice(category_list),
         "tags": random.sample(dummy_tags, random.randint(1, len(dummy_tags))),
-        "progress": random.int(0, 100),
+        "progress": random.randint(0, 100),
         "serious": random.randint(0, 5),
         "details": msg,
     }
