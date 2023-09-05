98819d8b6b23a7b711694c36eeeab6e6f2c08ab4
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-05 20:52:18 +0900

    [ADD] chatのためのopenapiとtempalteの雛形作成

---
+++ app/chat/get_all_chat_history.py
@@ -0,0 +1,27 @@
+import json
+
+
+def lambda_handler(event, context):
+    qsp = event.get("queryStringParameters")
+    print(qsp)
+    # ここに処理を書く
+    example = [
+        {
+            "chat_id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+            "user_id": "4f73ab32-21bf-47ef-a119-fa024bc2b9cc",
+            "timestamp": "2020-01-01T00:00:00+09:00",
+            "message": "こんにちは",
+            "created_at": "2020-01-01T00:00:00+09:00",
+            "updated_at": "2020-01-01T00:00:00+09:00",
+        }
+    ]
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(example),
+        "headers": {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Content-Type,X-CSRF-TOKEN",
+        },
+    }
