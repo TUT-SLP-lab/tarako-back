dcf2b9bf98bf6016fae9d1efed73c67f5a4ae119
Hansyo <hansyo.souti.1999@gmail.com>
2023-09-08 03:02:26 +0900

    update: add validation to get_user

--- app/users/get_user.py
+++ app/users/get_user.py
@@ -1,20 +1,21 @@
 from responses import get_response
-from table_utils import DynamoDBError, get_item, json_dumps, user_table
+from table_utils import get_item, json_dumps, user_table
+from validation import validate_user_id_not_none
 
 
 def lambda_handler(event, context):
     user_id = event.get("pathParameters", {}).get("user_id")
 
     # バリデーション
-    if not user_id:
-        return get_response(400, "Bad Request: Invalid path parameters")
+    is_valid, err_msg = validate_user_id_not_none(user_id)
+    if not is_valid:
+        return get_response(400, err_msg)
 
-    # ここに処理を書く
     try:
         response = get_item(user_table, "user_id", user_id)
-    except DynamoDBError as e:
-        return get_response(500, e)
     except IndexError as e:
         return get_response(404, e)
+    except Exception as e:
+        return get_response(500, e)
 
     return get_response(200, json_dumps(response))
